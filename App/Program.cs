5d76907eb535ab4f71e0bc1729b28aa0682c2f19
Dmytro Mykytenko <50519414+dmykyten@users.noreply.github.com>
2019-12-01 15:36:01 +0200

    Initial commit

---
+++ App/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+//using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleContainers;
+
+namespace App
+{
+    class Program
+    {
+        static void PrintList<T>(List<T> current)
+        {
+            while (current != null)
+            {
+                Console.WriteLine(current.Item);
+                current = current.Next;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var item1 = new List<int>(50);
+            var item2  = item1.Append(60);
+            var item3 = item2.Append(70);
+            PrintList(item1);
+            Console.ReadKey();
+            Console.WriteLine("====================================");
+
+            item1.Append(51);
+            PrintList(item1);
+            Console.ReadKey();
+
+            Console.WriteLine("====================================");
+
+            item1
+                .Next
+                .Next
+                .Append(65)
+                .Insert(63);
+
+
+            PrintList(item1);
+            Console.ReadKey();
+
+            Console.WriteLine("====================================");
+
+            item1 = item1.Insert(45);
+
+
+            PrintList(item1);
+            Console.ReadKey();
+
+            Console.WriteLine("====================================");
+            item3
+                .Previous
+                .Delete();
+
+            item3.Delete();
+
+
+
+            PrintList(item1);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ SimpleContainers/List.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleContainers
+{
+    public class List<T>
+    {
+        public List<T> Previous { get; set; }
+        public List<T> Next { get; set; }
+        public List(T item) {
+            Previous = null;
+            Next = null;
+            this.Item = item;
+        }
+        public T Item { get; set; }
+        public List<T> Append(T Item)
+        {
+            var temp = Next;
+            Next = new List<T>(Item);
+            this.Next.Next = temp;
+            this.Next.Previous = this;
+            if (temp != null) {
+                temp.Previous = Next;
+            }
+            return Next;
+        }
+
+        public List<T> Insert__(T Item) {
+            var temp = Previous;
+            var temp1 = Previous.Next;
+            Previous = new List<T>(Item);
+            Previous.Previous = temp;
+            Previous.Previous.Next = Previous;
+            Previous.Next = temp1;
+            return Next.Next;
+        }
+
+
+        public List<T> Insert(T Item)
+        {
+            var temp = Previous;
+            Previous = new List<T>(Item);
+            this.Previous.Previous = temp;
+            this.Previous.Next = this;
+            if (temp != null)
+            {
+                temp.Next = Previous;
+            }      
+            return Previous;
+        }
+
+        public void Delete() {
+            if (Previous != null)
+            {
+                Previous.Next = Next;
+            }
+            if (Next != null)
+            {
+                Next.Previous = Previous;
+            }
+            Previous = null;
+            Next = null;
+        }
+
+        public override string ToString()
+        {
+            var prevValue = Previous == null ? "null" : Previous.Item.ToString();
+            var nextValue = Next == null ? "null" : Next.Item.ToString();
+            return $"List(prev={prevValue},{Item},next={nextValue})";
+        }
+    }
+}
